6b9375317df2193c3b69a6cb9e69beb64640fe06
Ahmed Fekry <ahmedfekry792@gmail.com>
2023-03-03 05:27:18 +0200

    Returning missing File "AddNewAddressViewController.swift"

---
+++ E-commerce/Modules/AddNewAddress/View/AddNewAddressViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddNewAddressViewController.swift
+//  E-commerce
+//
+//  Created by Mina on 01/03/2023.
+//
+import UIKit
+
+class AddNewAddressViewController: UIViewController {
+
+    @IBOutlet weak var Countrylbl: UILabel!
+    @IBOutlet weak var Addresslbl: UILabel!
+    @IBOutlet weak var Phonelbl: UILabel!
+    @IBOutlet weak var CountryTF: UITextField!
+    @IBOutlet weak var AddressTF: UITextField!
+    @IBOutlet weak var PhoneTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func AddNewAddressBtn(_ sender: Any) {
+        
+    }
+    
+
+}
