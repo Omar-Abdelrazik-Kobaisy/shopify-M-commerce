5f5df521c1bdd166d3cf9ee33e3d947c8508c683
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2023-03-06 13:25:29 +0200

    modify

--- E-commerce/Modules/Products/View/ProductsViewController.swift
+++ E-commerce/Modules/Products/View/ProductsViewController.swift
@@ -78,14 +78,6 @@ class ProductsViewController: UIViewController , UICollectionViewDelegate , UICo
             return CGSize(width:self.view.frame.width*0.25, height: self.view.frame.height*0.25)
         }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
