dc60168d9e64c4d96e2d5dd2515004b30647d68f
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2023-03-08 16:05:15 +0200

    add postOrderFunction to NetworkManager

--- E-commerce/GenericModel/NetworkServices/ApiService.swift
+++ E-commerce/GenericModel/NetworkServices/ApiService.swift
@@ -11,6 +11,9 @@ import Alamofire
 protocol NetworkService
 {
     static func fetchFromApi<T : Decodable>( API_URL:String ,completion : @escaping (T?)-> Void)
+    
+    static func postOrderToApi(order : PostOrder)
+    
 }
 
 
@@ -34,4 +37,31 @@ class ApiService : NetworkService
     }
     
     
+    static func postOrderToApi(order: PostOrder) {
+        let url = URL(string: "https://12cda6f78842e3d15dd501d7e1fbc322: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@mad-ios1.myshopify.com/admin/api/2023-01/orders.json")
+        
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpShouldHandleCookies = false
+        var dictionary : [String:Any] = [:]
+                    //this comment made by me --> dont forget order.convertToDictionary() <--
+        do {
+            let bodyDictionary = try JSONSerialization.data(withJSONObject: dictionary,options: .prettyPrinted)
+
+            urlRequest.httpBody = bodyDictionary
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        } catch let error {
+            print(error.localizedDescription)
+            print("catch")
+        }
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if (data != nil && data?.count != 0){
+                let response = String(data:data!,encoding: .utf8)
+                print(response!)
+                }
+               }.resume()
+    }
+  
 }
