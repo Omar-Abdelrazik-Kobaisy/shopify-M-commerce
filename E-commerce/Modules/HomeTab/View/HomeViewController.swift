552234047ab4a604eeec081b75b1b640cf473986
kariman eltaweel <kariman.hesham0@gmail.com>
2023-03-08 02:15:29 +0200

    search for brands

--- E-commerce/Modules/HomeTab/View/HomeViewController.swift
+++ E-commerce/Modules/HomeTab/View/HomeViewController.swift
@@ -10,17 +10,33 @@ import Kingfisher
 
 class HomeViewController: UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
     
+  
+    
+    
     
     @IBOutlet weak var Ads_CollectionV: UICollectionView!
     @IBOutlet weak var Brands_CollectionV: UICollectionView!
     
     var brands:Brands?
     var viewModel = HomeViewModel()
-    
     var couponArr :[coupon]?
+    var brandFilter : [BrandItem] = []
+
+    
+    let searchController = UISearchController(searchResultsController: nil)
+
+    var isSearchBarEmpty: Bool{
+            return searchController.searchBar.text!.isEmpty
+        }
+    var isFiltering : Bool{
+            return searchController.isActive && !isSearchBarEmpty
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesSearchBarWhenScrolling = false
+        makeSearchBar()
+        
 
         viewModel.bindingBrands = { [weak self] in
             self!.brands = self!.viewModel.ObservableBrands
@@ -39,6 +55,10 @@ class HomeViewController: UIViewController ,UICollectionViewDelegate,UICollectio
         {
             return couponArr!.count
         }
+        //----------search-----------array---------filtered---------------
+        else if isFiltering {
+            return brandFilter.count
+        }
         return brands?.smart_collections.count ?? 0
     }
     
@@ -51,6 +71,16 @@ class HomeViewController: UIViewController ,UICollectionViewDelegate,UICollectio
             
             cell.Ad_imageV.image = couponArr![indexPath.row].img
             
+            return cell
+        }
+        //----------search-----------array---------filtered---------------
+        else if isFiltering {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "brand", for: indexPath) as! BrandsCollectionViewCell
+            
+            cell.Brand_imageV.kf.setImage(with: URL(string: brandFilter[indexPath.row].image.src ?? ""))
+            cell.Brand_title.text = brandFilter[indexPath.row].title
+
             return cell
         }
         
@@ -104,5 +134,30 @@ class HomeViewController: UIViewController ,UICollectionViewDelegate,UICollectio
         let favouite = self.storyboard?.instantiateViewController(withIdentifier: "WishListViewController")as!WishListViewController
         
         navigationController?.pushViewController(favouite, animated: true)
+    }
+    
+}
+//------------------for-------------search-----------------
+extension HomeViewController: UISearchResultsUpdating, UISearchBarDelegate{
+
+    func makeSearchBar(){
+
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.tintColor = .black
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for Brand"
+        let textFieldInsideSearchBar = searchController.searchBar.value(forKey: "searchField") as? UITextField
+
+        textFieldInsideSearchBar?.textColor = .black
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+                
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        brandFilter = (brands?.smart_collections)!.filter({ filter in
+            return filter.title!.lowercased().contains(searchController.searchBar.text!.lowercased())
+        })
+        Brands_CollectionV.reloadData()
     }
 }
