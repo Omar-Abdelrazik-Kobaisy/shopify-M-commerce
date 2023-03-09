dfc58eefaf9d9b5de0b694f5613869da3a4511c7
kariman eltaweel <kariman.hesham0@gmail.com>
2023-03-09 20:39:01 +0200

    WishListScreen

--- E-commerce/Modules/WishList/View/WishListViewController.swift
+++ E-commerce/Modules/WishList/View/WishListViewController.swift
@@ -9,81 +9,79 @@ import UIKit
 
 class WishListViewController: UITableViewController {
 
+    @IBOutlet var wishListTable: UITableView!
+    var wishListArray :[FavoriteProduct] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        wishListTable.reloadData()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
+    override func viewWillAppear(_ animated: Bool) {
+        
+        wishListArray = CoreDataManager.fetchFromCoreData()
+        wishListTable.reloadData()
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+     
+        return wishListArray.count
     }
+    
+    
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WishlistCell", for: indexPath)
+        
+        cell.textLabel?.text = wishListArray[indexPath.row].product_title
+        let url = URL(string: wishListArray[indexPath.row].product_img ?? "")
+        cell.imageView?.kf.setImage(with: url)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+            showAlert(indexPath: indexPath)
+        }
 
     }
-    */
+    func showAlert(indexPath: IndexPath){
+        // declare Alert
+        let alert = UIAlertController(title: "Delete", message: "are you sure about deletion", preferredStyle: .alert)
+        
+        
+        
+        //AddAction
+        alert.addAction(UIAlertAction(title: "OK", style: .default , handler: { [self] action in
+            print("ok clicked")
+            guard let key = wishListArray[indexPath.row].product_id else
+            { return  }
+            print("\( wishListArray[indexPath.row].product_id ?? 0)")
+            UserDefaults.standard.set(false, forKey: "\(key)")
+            CoreDataManager.deleteFromCoreData(productId: wishListArray[indexPath.row].product_id ?? 0)
+            print(wishListArray[indexPath.row].product_id!)
+            wishListArray.remove(at: indexPath.row)
+            wishListTable.deleteRows(at: [indexPath], with: .fade)
+            wishListTable.reloadData()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel , handler: { action in
+            print("Cancel clicked")
+        }))
+        
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+        //showAlert
+        self.present(alert, animated: true) {
+            print("alert done")
+        }
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
